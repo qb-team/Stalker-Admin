fc350865e4c70254356c5e4f03d717df24d44f46
Cis8 <ema.cis8@gmail.com>
2020-03-28 16:41:58 +0100

    micro modifiche ai routes di reset psw

---
+++ src/app/reset-password/reset-password.routes.ts
@@ -0,0 +1,7 @@
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent} from '../login/login.component';
+
+
+export const LoginRoutes: Routes = [
+  { path: 'Login', component: LoginComponent}
+];
