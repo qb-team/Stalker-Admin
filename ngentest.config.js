7e7618805aab1824465f125b5cea9b608ef8e54d
federico.p98@outlook.it <53578288+pheasant98@users.noreply.github.com>
2020-04-02 10:24:53 +0200

    aggiunto test auto gen menubar

---
+++ ngentest.config.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const path = require('path');
+const klassTemplate = fs.readFileSync(path.join(__dirname, 'src', 'class', 'class.template.ts.ejs'), 'utf8');
+const componentTemplate = fs.readFileSync(path.join(__dirname, 'src', 'component', 'component.template.ts.ejs'), 'utf8');
+const directiveTemplate = fs.readFileSync(path.join(__dirname, 'src', 'directive', 'directive.template.ts.ejs'), 'utf8');
+const injectableTemplate = fs.readFileSync(path.join(__dirname, 'src', 'injectable', 'injectable.template.ts.ejs'), 'utf8');
+const pipeTemplate = fs.readFileSync(path.join(__dirname, 'src', 'pipe', 'pipe.template.ts.ejs'), 'utf8');
+
+module.exports = {
+   framework: 'karma', // or 'jest'
+  // .spec file EJS templtes
+  templates: {
+    klass: klassTemplate,
+    component: componentTemplate,
+    directive: directiveTemplate,
+    injectable: injectableTemplate,
+    pipe: pipeTemplate
+  },
+  // necessary directives used for a component test
+  directives: [
+    'oneviewPermitted'
+  ],
+  // necessary pipes used for a component test
+  pipes: [
+    'translate', 'phoneNumber', 'safeHtml'
+  ],
+  // when convert to JS, some codes need to be replaced to work
+  replacements: [
+    { from: 'require\\("html-custom-element"\\)', to: '{}'}, // some 3rd party require statements causes error, using import or windows directly
+    { from: '^\\S+\\.define\\(.*\\);', to: ''} // some commands causes error
+  ],
+  // when constructor typs is as following, create a mock class with this properties
+  // e.g. @Injectable() MockElementRef { nativeElement = {}; }
+  providerMocks: {
+    ElementRef: ['nativeElement = {};'],
+    Router: ['navigate() {};'],
+    Document: ['querySelector() {};'],
+    HttpClient: ['post() {};'],
+    TranslateService: ['translate() {};'],
+    EncryptionService: [],
+  },
+  // when ngentest runs with a directory, include only these files
+  includeMatch: [/(component|directive|pipe|service).ts/],
+  // when ngentest runs with a directory, exclude these files
+  excludeMatch: []
+}
--- src/app/control-panel/menubar/menubar.component.spec.ts
+++ src/app/control-panel/menubar/menubar.component.spec.ts
@@ -1,34 +1,121 @@
+// tslint:disable
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Pipe, PipeTransform, Injectable, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA, Directive, Input, Output } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { Observable, of as observableOf, throwError } from 'rxjs';
 
+import { Component } from '@angular/core';
 import { MenubarComponent } from './menubar.component';
-import {DataService} from '../../services/data.service';
-import {AngularFireAuth} from '@angular/fire/auth';
-import {AngularFireModule} from '@angular/fire';
-import {environment} from '../../../environments/environment';
+import { DataService } from '../../services/data.service';
+import { AuthenticationService } from '../../services/authentication.service';
+import { OrganizationService } from 'src/api/api';
+import { Router } from '@angular/router';
+
+@Injectable()
+class MockDataService {}
+
+@Injectable()
+class MockAuthenticationService {
+  SignOut(){}
+}
+
+@Injectable()
+class MockOrganizationService {
+  getOrganizationList(){}
+}
+
+@Injectable()
+class MockRouter {
+  navigate() {};
+  navigateByUrl(url: string) { return url; }
+}
+
+@Directive({ selector: '[oneviewPermitted]' })
+class OneviewPermittedDirective {
+  @Input() oneviewPermitted;
+}
 
 describe('MenubarComponent', () => {
-  let component: MenubarComponent;
-  let fixture: ComponentFixture<MenubarComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        AngularFireModule.initializeApp(environment.firebase)
-      ],
-      declarations: [ MenubarComponent ],
-      providers: [ DataService,
-        AngularFireAuth ]
-    })
-    .compileComponents();
-  }));
+  let fixture;
+  let component;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ FormsModule, ReactiveFormsModule ],
+      declarations: [
+        MenubarComponent,
+        OneviewPermittedDirective
+      ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA ],
+      providers: [
+        { provide: DataService, useClass: MockDataService },
+        { provide: AuthenticationService, useClass: MockAuthenticationService },
+        { provide: OrganizationService, useClass: MockOrganizationService },
+        { provide: Router, useClass: MockRouter }
+      ]
+    }).overrideComponent(MenubarComponent, {
+
+    }).compileComponents();
     fixture = TestBed.createComponent(MenubarComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = fixture.debugElement.componentInstance;
   });
 
-  /*it('should create', () => {
+  afterEach(() => {
+    component.ngOnDestroy = function() {};
+    fixture.destroy();
+  });
+
+  it('should run #constructor()', async () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should run #ngOnInit()', async () => {
+    component.os = component.os || {};
+    spyOn(component.os, 'getOrganizationList').and.returnValue(observableOf({}));
+    component.orgArr = component.orgArr || {};
+    component.orgArr = '0';
+    component.ds = component.ds || {};
+    component.ds.org = {
+      emit: function() {}
+    };
+    component.ngOnInit();
+     expect(component.os.getOrganizationList).toHaveBeenCalled();
+  });
+
+  /*it('should run #setOrg()', async () => {
+    component.ds = component.ds || {};
+    component.ds.org = {
+      emit: function() {}
+    };
+    component.setOrg({});
+
   });*/
+
+  it('should run #SignOut()', async () => {
+    component.authenticationService = component.authenticationService || {};
+    spyOn(component.authenticationService, 'SignOut');
+    spyOn(component, 'navigateToLogin');
+    component.SignOut();
+    expect(component.authenticationService.SignOut).toHaveBeenCalled();
+    expect(component.navigateToLogin).toHaveBeenCalled();
+  });
+
+  it('should run #homePage()', async () => {
+    component.ds = component.ds || {};
+    component.ds.active_content = {
+      emit: function() {}
+    };
+    component.homePage();
+
+  });
+
+  it('should run #navigateToLogin()', async () => {
+    spyOn(component.router, 'navigateByUrl');
+    component.router = component.router || {};
+    component.navigateToLogin();
+    expect(component.router.navigateByUrl).toHaveBeenCalled();
+  });
+
 });
