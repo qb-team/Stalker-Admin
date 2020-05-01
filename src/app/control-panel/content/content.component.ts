c856fdb510fb685f306315d0250bf34d71227f6b
federico.p98@outlook.it <53578288+pheasant98@users.noreply.github.com>
2020-05-01 17:18:08 +0200

    fix content.component

--- src/app/control-panel/content/content.component.ts
+++ src/app/control-panel/content/content.component.ts
@@ -12,30 +12,15 @@ import {AdministratorDataService} from '../../services/AdministratorData.service
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
-  /*
-  * The actually selected organization
-  */
-  private actualOrganization: Organization;
-
-  constructor( private ads: AdministratorDataService, private activatedRoute: ActivatedRoute ) { console.log('Constructor content component'); }
 
 
-  get getOganization(): Organization {
-    return this.actualOrganization;
-  }
+  constructor( ) { console.log('Constructor content component'); }
 
-  set setOrganization(value: Organization) {
-    this.actualOrganization = value;
-  }
-  /*
-  * Subscribes to the organization service and to active_content, in order to retrive informations about the selected organization and dinamically update the specific-content component
-  * Finally, it sets the active specific-component to the home page content component (content-home)
-  */
+
+
   ngOnInit() {
-    this.subscribeToOrganization();
+
   }
 
-  subscribeToOrganization(): void {
-    this.ads.getOrganization.subscribe((o: Organization) => {this.actualOrganization = o; });
-  }
+
 }
