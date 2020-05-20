ca108c1b6d394c5078009ce4bcffcf66b8623fb3
federico.p98@outlook.it <53578288+pheasant98@users.noreply.github.com>
2020-05-20 11:25:14 +0200

    fix

--- src/app/services/owner-guard.service.ts
+++ src/app/services/owner-guard.service.ts
@@ -15,8 +15,8 @@ export class OwnerGuardService implements CanActivate {
 
   canActivate(): Observable<boolean> |  Promise<boolean> | boolean {
     console.log('perm');
-    this.aod.getOrganization.subscribe( (org: Organization) => {
-      this.apd.getUserPermissions().subscribe( (perm: Permission[]) => {
+    this.aod.getOrganization.subscribe((org: Organization) => {
+      this.apd.getUserPermissions().subscribe((perm: Permission[]) => {
         let flag = false;
         let index = 0;
         console.log('subsub');
@@ -29,17 +29,19 @@ export class OwnerGuardService implements CanActivate {
           }
         }
         localStorage.setItem('perm', perm[index].permission.toString());
-        if ( perm[index].permission === 3) {
+        if (perm[index].permission === 3) {
           console.log('CanActivate: true because flag from localStorage (key) === true');
         } else {
-          this.value = false;
           console.log('Back to login because cannot activate');
-          this.router.navigateByUrl('/Content-panel');
-          return false;
         }
       });
-  });
-      // const flag = localStorage.getItem('key');
-    return localStorage.getItem('perm') === '3';
+    });
+    // const flag = localStorage.getItem('key');
+    if (localStorage.getItem('perm') === '3') {
+      return true;
+    } else {
+      this.router.navigateByUrl('/Content-panel');
+      return false;
+    }
   }
 }
